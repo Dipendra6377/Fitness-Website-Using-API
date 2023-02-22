105448928e5c14213f37f5b9b334294292c49902
Dipendra Singh <91822875+Dipendra6377@users.noreply.github.com>
2023-02-22 21:11:47 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercise from "../components/SearchExercise";
+import Exercises from "../components/Exercises";
+
+const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercise
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  );
+};
+
+export default Home;
